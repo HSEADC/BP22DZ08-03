74f62cec51dc5f48ec82aa80b823249899f0d77f
BorisTemchur <bulgakovboris424@gmail.com>
2024-06-29 11:14:34 +0300

    Шапка

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './index.css'
+const searchButton = document.querySelector('.search button');
+searchButton.addEventListener('click', function() {
+  const searchTerm = document.querySelector('.search input').value;
+  alert('You searched for: ' + searchTerm);
+});
+
